c305646ec22dc81e63b43a445e899372d9d882ae
Alexander <alexander.martinez@lapatota.cl>
2022-08-14 00:27:33 -0400

    SearchComponent

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,27 @@
+import querystring from 'query-string'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components/HeroCard"
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation()
+
+  // paquete instalado para manejar querys
+  const { q = '' } = querystring.parse(location.search)
+
+  const { searchText, onInputChange } = useForm({
+    searchText: ''
+  })
+
+  const onSeachSubmit = (e) => {
+    e.preventDefault()
+    if (searchText.trim().lenght <= 1) return
+
+    navigate(`?q=${searchText}`)
+  }
+
   return (
     <>
       <h1>SearchPage</h1>
@@ -10,12 +31,15 @@ export const SearchPage = () => {
           <h4>Searching</h4>
           <hr />
 
-          <form>
+          <form onSubmit={onSeachSubmit}>
             <input
               type="text"
               className="form-control"
               placeholder="SearchText"
               autoComplete="off"
+              name="searchText"
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className="btn btn-outline-primary mt-1">
@@ -34,7 +58,7 @@ export const SearchPage = () => {
           </div>
 
           <div className="alert alert-danger">
-            No hero width <strong>ABC</strong>
+            No hero width <strong>{q}</strong>
           </div>
 
           {/* <HeroCard  /> */}
