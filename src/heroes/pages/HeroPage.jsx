76831511677070776981fe39587c87c760391d71
Alexander <alexander.martinez@lapatota.cl>
2022-08-13 23:33:58 -0400

    fix navigate render

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,5 @@
 
+import { Navigate } from "react-router-dom"
 import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
