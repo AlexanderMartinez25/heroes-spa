388ecd827181721a1739aa7aa67169230657e148
Alexander <alexander.martinez@lapatota.cl>
2022-08-13 21:19:16 -0400

    Leer argumentos por URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,21 @@
+import { Navigate } from "react-router-dom"
+import { useParams } from 'react-router-dom'
+import { getHeroById } from '../helpers'
 
 export const HeroPage = () => {
-    return (
-        <>
-            <h1>HeroPage</h1>
-        </>
-    )
+
+  // obtenemos los parámetros de la ruta
+  const { id } = useParams()
+
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to='/marvel' />
+  }
+
+  return (
+    <>
+      <h1>HeroPage</h1>
+    </>
+  )
 }
